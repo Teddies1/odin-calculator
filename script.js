f6d469b2a5675761e947d16282ff166aecfadb4b
Teddies1 <nghonjoo@gmail.com>
2024-05-14 16:57:43 +0800

    removed commented code

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// var one, operator, two;    
-// var oneFlag = 0;
-// var twoFlag = 0;
-// var operatorFlag = 0;
-// var displayText = document.querySelector(".result");
 
 const add = (one, two) => {
     return one + two;
@@ -37,62 +32,6 @@ const operate = (one, two, operator) => {
         return divide(one, two);
     }
 }
-
-// const calculator = () => {
-//     var buttons = document.querySelectorAll("button");
-//     buttons.forEach((button) => {
-//         button.addEventListener("click", (e) => {
-//             let buttonClass = e.target.classList;
-//             if (buttonClass.contains("number")){
-//                 clickNumber(e);
-//             }
-//             else if (buttonClass.contains("operation")){
-//                 if (operatorFlag === 0){
-//                     clickOperator(e);
-//                 }
-//             }
-//             else if (buttonClass.contains("equal")){
-//                 if (oneFlag === 1 && operatorFlag === 1 && !isNaN(two)){
-//                     twoFlag = 1;
-//                     let ans = operate(one, two, operator);
-//                     displayAns(ans);
-//                 }
-//             }
-//             else if (buttonClass.contains("clear")){
-//                 displayText.innerHTML = "";
-//                 oneFlag = 0;
-//                 twoFlag = 0;
-//                 operatorFlag = 0;
-//             }
-//         });
-//     });
-// }
-
-// const displayAns = (ans) => {
-//     displayText.textContent = ans;
-// }
-
-// const clickNumber = (e) => {
-//     if (oneFlag === 0){
-//         displayText.textContent += e.target.textContent;
-//         one = +displayText.textContent;
-//     }
-//     else if (twoFlag === 0){
-//         if (isNaN(+displayText.textContent)){
-//             displayText.innerHTML = "";
-//         }
-//         displayText.textContent += e.target.textContent;
-//         two = +displayText.textContent;
-//     }
-    
-// }
-
-// const clickOperator = (e) => {
-//     operator = e.target.textContent;
-//     displayText.textContent = operator;
-//     oneFlag = 1;
-//     operatorFlag = 1;
-// }
 
 const calculator = () => {
     var one, operator, two;    
